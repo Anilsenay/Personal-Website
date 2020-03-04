34daa50e94b47717ec1452456d5dbb1275263861
Anıl Şenay <anilsenay3@gmail.com>
2020-03-05 00:51:27 +0300

    social media links fixed

--- src/GlowingButton.js
+++ src/GlowingButton.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class GlowingButton extends Component {
 
     state = {
-        isGlow:false
+        isGlow:false,
+        twitter:"https://twitter.com/anilsenay",
+        instagram:"https://instagram.com/anilsenay",
+        github:"https://github.com/anilsenay",
+        linkedin:"https://linkedin.com/in/anılşenay"
     };
 
     toggleBox = () => {
@@ -12,11 +16,13 @@ export default class GlowingButton extends Component {
 
     render() {
         const { isGlow } = this.state;
+
+        console.log(this.state['twitter']);
         return (
             <div className={`buttons-display ${isGlow ? "test": ""}`}
                 onMouseEnter={() => {this.toggleBox()}}
                 onMouseLeave={() => {this.toggleBox()}}>
-                <a href="https://twitter.com/anilsenay" className="buttons">
+                <a href={this.state[this.props.social]} className="buttons">
                     <div className="box" ><i className={`fab fa-${this.props.social} icon-margin`} aria-hidden="true"/>
                         <span>{this.props.text}</span>
                     </div>
