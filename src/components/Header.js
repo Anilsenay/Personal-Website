80b30b52a5ed96f4ccf7c41a02142b2c6c886af5
Anıl Şenay <anilsenay3@gmail.com>
2020-03-08 16:04:06 +0300

    changed header <a> tags to <Link> tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <a href="/" className="header-links">/home</a>
-                <a href="/projects" className="header-links">/projects</a>
+                <Link to="/" className="header-links">/home</Link>
+                <Link to="/projects" className="header-links">/projects</Link>
           </div>
         )
     }
